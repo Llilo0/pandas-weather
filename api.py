daa695ff9e67afdcd4b3fdd944357f04280380e5
Llilo0 <ilona.zajonc@gmail.com>
2025-07-09 19:49:37 +0200

    tools implementation

--- api.py
+++ api.py
@@ -4,6 +4,9 @@ from http.client import responses
 from config import Settings
 import requests
 from datetime import datetime
+from tools import wind_speed
+from tools import temp_converter
+
 
 
 def fetch_weather ():
@@ -14,16 +17,19 @@ def fetch_weather ():
         weather = response.json()
 
         data = {
-            "Odczuwalna": weather["main"]["feels_like"],
+            "Odczuwalna": temp_converter(weather["main"]["feels_like"]),
             "Ciśnienie": weather["main"]["pressure"],
             "Wilgotność": weather["main"]["humidity"],
-            "Zwykla temperatura": weather["main"]["temp"],
+            "Zwykla temperatura": temp_converter(weather["main"]["temp"]),
             "Opis pogody": weather["weather"][0]["description"],
             "Miejsce": weather["name"],
-            "Prędkosc wiatru": weather["wind"]["speed"],
+            "Prędkosc wiatru": wind_speed(weather["wind"]["speed"]),
             "Data pomiaru": datetime.now()
         }
+
         return data
 
     except Exception as e:
         print("Wystąpił błąd:", e)
+
+print(fetch_weather())
---
+++ tools.py
@@ -0,0 +1,7 @@
+def wind_speed(x):
+    result = x * 3.6
+    return round(result,2)
+
+def temp_converter(t):
+    result = t - 273.15
+    return round(result,2)
